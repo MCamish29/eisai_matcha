b3a6adf2994795b23066cccd2fecf78bfa326c4c
MCamish29 <mcamish29@gmail.com>
2025-03-16 09:53:04 +0000

    add products views and templates

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from itertools import chain
 
-# Create your views here.
+from .models import Tea, Equipment, Kit
+
+def all_products(request):
+    """A view to return all products page, including sorting and search queries"""
+
+    teas = Tea.objects.all()
+    equipment = Equipment.objects.all()
+    kits = Kit.objects.all()
+
+    all_products = list(chain(teas, equipment, kits))  # Merge all products into one list
+
+    context = {
+        'all_products': all_products,
+    }
+
+    return render(request, 'products/products.html', context)
