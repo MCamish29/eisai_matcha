14ac7e5c93a726e64a03c158459e04c15ef9cf8b
MCamish29 <mcamish29@gmail.com>
2025-03-16 17:15:29 +0000

    add search funtionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Tea, Equipment, Kit
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Tea, Equipment, Kit, Category, Country
 
 def all_products(request):
     """A view to return all products page"""
@@ -7,11 +9,26 @@ def all_products(request):
     teas = Tea.objects.all()
     equipment = Equipment.objects.all()
     kits = Kit.objects.all()
+    search = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            search = request.GET['q']
+            if not search:
+                messages.error(request, "No search criteria entered!")
+                return redirect(reverse('products'))
+            
+            # Perform search in category names, product names, and descriptions
+            queries = Q(category__category__icontains=search) | Q(product_name__icontains=search) | Q(description__icontains=search)
+            teas = teas.filter(queries)
+            equipment = equipment.filter(queries)
+            kits = kits.filter(queries)
 
     context = {
         'teas': teas,
         'equipment': equipment,
         'kits': kits,
+        'search_term': search,
     }
 
     return render(request, 'products/all_products.html', context)
